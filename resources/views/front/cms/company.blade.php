29827e7ad53958c1ba82cbfdd378fbdffdd9e229
khushali <khushaliloopcon@gmail.com>
2023-09-01 14:17:45 +0530

    Khushali : done : company-cms-route

---
+++ resources/views/front/cms/company.blade.php
@@ -0,0 +1,17 @@
+@extends('front.layout.main')
+@section('content')
+<!-- contact-us page start  -->
+<div class="contact-section-main">
+    <div class="container page-content">
+        <div class="row">
+            <div class="col-12 mx-auto my-lg-5 my-md-5 my-sm-3 my-3">
+                <div class="pt-lg-4 pt-md-4 pt-sm-3 pt-3">
+                    <h3 class="text-center page-head">{{$site_title}}</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- contact-us page end -->
+
+@endsection
