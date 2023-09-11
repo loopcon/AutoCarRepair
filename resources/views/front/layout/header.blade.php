1c07e4ef1d2b98792996dc5ce6748ae5d9637b87
loopconit <loopconinfo@gmail.com>
2023-09-11 13:19:33 +0530

    savan: css change  design

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -84,21 +84,6 @@
                     <?php /* <img data-bs-toggle="modal" data-bs-target="#searchbarModal"  src="{{ asset('front/img/navbar-search-icon.png') }}" alt=""> */ ?>
                      <span class="search-svg-main" data-bs-toggle="modal" data-bs-target="#searchbarModal" ><i class="fa-solid fa-magnifying-glass"></i></span>
                 </div>
-                <div class="modal fade" id="searchbarModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog searchbar-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="input-group mb-3">
-                                <input type="text" class="form-control search_text" placeholder="Search" value="{{Request::get('search')}}" aria-label="Recipient's username" aria-describedby="basic-addon2">
-                              </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                
                 <div>
                     <a class="navbar-appointment-btn apt-btn" href="javascript:void(0)" >Book A Service</a>
                 </div>
@@ -175,7 +160,7 @@
                             <i class="fa-solid fa-magnifying-glass"></i>
                         </div>
                     </div>
-                    <div class="row m-0" id="amodal_brands">
+                    <div class="row m-0 select-brand-imgmain" id="amodal_brands">
                         @php($brands = getbrands())
                         @if($brands->count())
                             @foreach($brands as $brand)
@@ -286,3 +271,17 @@
 </div>
 <!-- Appointment Number modal -->
 <!-- navbar end  -->
+<div class="modal fade" id="searchbarModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog searchbar-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control search_text" placeholder="Search" value="{{Request::get('search')}}" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
