e4f3ec3c4d2900456a8a05746f3254e7615da6f5
Disha <loopcon8@gmail.com>
2023-09-12 09:50:30 +0530

    disha:download csv in product module

--- app/Imports/ImportProduct.php
+++ app/Imports/ImportProduct.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Product;
+use App\Models\ProductImage;
 use App\Models\ShopCategory;
 use Maatwebsite\Excel\Concerns\ToModel;
 
@@ -33,7 +34,24 @@ class ImportProduct implements ToModel
         }
 
 
-        return new Product([
+        // return new Product([
+        //     'slug' => strtolower($row[8]),
+        //     'shop_category_id' => $category_id,
+        //     'name' => $row[1],
+        //     'sku' => $row[7],
+        //     'description' => $row[2],
+        //     'specification' => $row[3],
+        //     'price' => $row[6],
+        //     'amazon_link' => $row[4],
+        //     'flipcart_link' => $row[5],
+        //     'meta_title' => $row[9],
+        //     'meta_keywords' => $row[10],
+        //     'meta_description' => $row[11],
+        //     'is_archive' => 1,
+        //     'status' => 1,
+        // ]);
+
+        $product = new Product([
             'slug' => strtolower($row[8]),
             'shop_category_id' => $category_id,
             'name' => $row[1],
@@ -49,5 +67,24 @@ class ImportProduct implements ToModel
             'is_archive' => 1,
             'status' => 1,
         ]);
+        if($product)
+        {
+            $jsonobj = '{"is_primary":1,"image": "https://acr-mechanic.com/uploads/content/banner16930250321693031029.webp","image_title": ""gfgf},{"is_primary": 0,"image": "https://acr-mechanic.com/uploads/content/banner16930250321693031029.webp","image_title": "dff"},{"is_primary": 0,"image": "https://acr-mechanic.com/uploads/content/banner16930250321693031029.webp","image_title": "dssd"}';
+            $data = json_decode($jsonobj,true);
+            // foreach($data as $image){
+            //     // $image_data = new ProductImage([
+            //     //     'product_id' => $product->id,
+            //     //     'is_primary' => $image->is_primary,
+            //     //     'image'=> $image->image,
+            //     //     'image_title' => $image->image_title,
+            //     // ]);
+            //     print_r($image);
+            // }
+            // echo "<pre>";
+            // print_r($row);
+            print_r($data);
+            // print_r(json_decode($row[12],true));
+        }
+        return $product;
     }
 }
--- resources/views/backend/product/export-csv.blade.php
+++ resources/views/backend/product/export-csv.blade.php
@@ -1,18 +1,19 @@
 <table border=".5pt">
     <thead>
         <tr>
-             <th>Shop Category</th>
-             <th>Product Name</th>
-             <th>Description</th>
-             <th>Specification</th>
-             <th>Amazon Link</th>
-             <th>Flipcart Link</th>
-             <th>Price</th>
-             <th>Sku</th>
-             <th>Slug</th>
-             <th>Meta Title</th>
-             <th>Meta Keyword</th>
-             <th>Meta Description</th>
+            <td>abc</td>
+            <td>auto service</td>
+            <td>dfgyfty</td>
+            <td>iguhioho</td>
+            <td>https://stackoverflow.com/questions/68794054/foreach-argument-must-be-of-type-arrayobject-null-given-in-laravel-8</td>
+            <td>https://stackoverflow.com/questions/68794054/foreach-argument-must-be-of-type-arrayobject-null-given-in-laravel-8</td>
+            <td>500</td>
+            <td>aaa</td>
+            <td>bbb</td>
+            <td>meta abc</td>
+            <td>jjj</td>
+            <td>hifhiofof</td>
+            <td>[{"is_primary":1,"image": "https://acr-mechanic.com/uploads/content/banner16930250321693031029.webp","image_title": ""gfgf},{"is_primary": 0,"image": "https://acr-mechanic.com/uploads/content/banner16930250321693031029.webp","image_title": "dff"},{"is_primary": 0,"image": "https://acr-mechanic.com/uploads/content/banner16930250321693031029.webp","image_title": "dssd"}]</td>
         </tr>
     </thead>
 </table>
