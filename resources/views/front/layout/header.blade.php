9b88b9da317596e089cddfa0a36ba253ea65f20e
Disha <loopcon8@gmail.com>
2023-09-25 10:42:57 +0530

    disha:show vehicle number in order detail in backend

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="{{isset($meta_description) ? $meta_description : ''}}">
     <meta name="keywords" content="{{isset($meta_keywords) ? $meta_keywords : ''}}">
-    <meta name="tag" content="{!! isset($extra_meta_tag) ? $extra_meta_tag : '' !!}">
-    <meta name="extra_meta_description" content="{!! isset($extra_meta_description) ? ($extra_meta_description) : '' !!}">
+    {!! isset($extra_meta_tag) ? $extra_meta_tag : '' !!}
+    <meta name="extra_meta_description" content="{!! isset($extra_meta_description) ? strip_tags($extra_meta_description) : '' !!}">
     <title>{{$site_title.' | '. $site_name}}</title>
     <link rel="icon"  href="{{ asset('public/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('front/css/bootstrap.min.css') }}">
